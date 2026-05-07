9f59ed38462b64c4202835a2cdce7076d90cad49
LI MING YU <LIMY.25@ichat.sp.edu.sg>
2026-05-07 15:18:37 +0800

    bmi test

--- test_Lab3.py
+++ test_Lab3.py
@@ -2,9 +2,17 @@ import Lab3
 
 print("Test_Lab3")
 
+def test_bubble_sort_more_than_10():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 88, 77, 66, 55]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert (result == 1)
+
+def test_bubble_sort_empty():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert (result == 0)
 
 def test_bubble_sort_ascending():
-    result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [11, 12, 22, 25, 34, 64, 90]
 
@@ -13,7 +21,6 @@ def test_bubble_sort_ascending():
     assert (result == test_arr)
 
 def test_bubble_sort_descending():
-    result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [90, 64, 34, 25, 22, 12, 11]
 
@@ -22,9 +29,13 @@ def test_bubble_sort_descending():
     assert (result == test_arr)
 
 def test_bubble_sort_invalid():
-    result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
 
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_non_integer():
+    input_arr = [64, 34, 25, 12, 22, 11, '90']
+    result =Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    assert (result == 2)
